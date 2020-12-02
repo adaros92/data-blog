b51b349c64c9faf9557a92c832131f641cb2905a
adamsrosales <adams.rosales.92@gmail.com>
2020-12-02 01:31:27 -0800

    Add working starter blog site

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Adams Rosales'
+SITENAME = 'Deciphering Big Data'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Static files to include with the site
+STATIC_PATHS = ['static']
+
+# Social widget
+GITHUB_URL = 'https://github.com/adaros92'
+LINKEDIN_URL = 'https://www.linkedin.com/in/adamsr09/'
+
+# Homepage header cover
+HEADER_COVER = 'static/masthead.jpg'
+
+DEFAULT_PAGINATION = False
+
+THEME = "/Users/adamsrosales/pelican-addon-clones/pelican-themes/clean-blog"
